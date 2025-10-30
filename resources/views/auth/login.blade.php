686d7971ddbfa44666ba2aca8962f776d4b6fdb3
Felipe Sakai <felipesakaiipolito@gmail.com>
2025-10-30 11:13:57 -0400

    Testing login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <h1>Login</h1>
+
+    <form action="/login" method="post">
+        @csrf
+
+        <input name="email" placeholder="Email" value="{{ old('email') }}" />
+        <input type="password" name="password" placeholder="Senha" />
+
+        <button>Logar</button>
+    </form>
+</div>
